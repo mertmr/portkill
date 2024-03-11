339ba274d70e9df91b5d7c7da881903e38eb9c89
Mert Meral <mertmr@gmail.com>
2024-03-11 10:23:43 +0400

    remove the flag and take the port parameter directly

--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 )
 
 func main() {
-	portFlag := flag.Int("port", 0, "Port number to kill the process on")
-	flag.Parse()
-
-	if *portFlag == 0 {
-		fmt.Println("Please specify a port using the -port flag.")
+	if len(os.Args) < 2 {
+		fmt.Println("Error: No port number provided.")
+		fmt.Println("Usage: portkill.exe <port_number>")
+		os.Exit(1)
+	}
+	
+	portStr := os.Args[1]
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		fmt.Println("Invalid port number.")
 		os.Exit(1)
 	}
 
-	port := *portFlag
-
-	// Check if the port is in use.
 	conn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", port))
 	if err == nil {
 		conn.Close()
-		// Kill the process using the port.
 		err = killProcessOnPort(port)
 		if err != nil {
 			fmt.Println(err)
